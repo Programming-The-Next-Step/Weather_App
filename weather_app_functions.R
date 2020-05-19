fc00456f61b3d8fd79859b7f33d5fb207a5962c4
Bertlich <64595164+Bertlich@users.noreply.github.com>
2020-05-19 13:42:21 +0200

    Update weather_app_functions.R

--- weather_app_functions.R
+++ weather_app_functions.R
@@ -6,17 +6,58 @@ library(dplyr)
 library(leaflet)
 library(mapview)
 library(magick)
-library(tidygeocoder)
 library(stringr)
-
-# tidygeocoder::geo_osm() uses a string with an address as input and returns
-# the latitude and longitude of the location. 
+# library(tidygeocoder)
 
 # you will need an API key to access the data from openweathermaps.org 
 # you have to sign up for a free account to get the api key 
 # the api key is private and should not be shared with other people
 
 my_api_key <- "insert your api key here" 
+
+# tidygeocoder::geo_osm() uses a string with an address as input and returns
+# the latitude and longitude of the location. The tidygeocoder::geo_osm() function was substituted with my own function geocode().
+
+# The function geocode() takes a location as input and returns the longitude and latitude of this location.
+# This way, I don't need to install and use the external function tidygeocoder::geo_osm() anymore. 
+
+geocode <- function(location){
+  
+  # split the location string, that the function got as input, into the different words. 
+  my_location <- stringr::str_split(location, boundary("word"))
+  my_location_string <- my_location[[1]][1]
+  
+  for(i in 2:length(my_location[[1]])){
+    my_location_string <- paste(my_location_string, "+", my_location[[1]][i], sep = "")
+  }
+  
+  # Write down the url that retrieves the long/lat information from openstreetmap.org
+  
+  my_url <- paste("https://nominatim.openstreetmap.org/search?q=", my_location_string, 
+                  "&format=json&limit=1", sep="")
+  
+  # call the Url, retrieve the data and check if it was successful. 
+  my_raw_results <- httr::GET(my_url)  
+  status_code(my_raw_results)
+  
+  if (status_code != 200) {
+    stop("Please enter a valid location!")
+  }
+  
+  # Transform the JSON data into useable format. 
+  my_content <- httr::content(my_raw_results, as = "text")
+  
+  my_content_from_json <- jsonlite::fromJSON(my_content)
+  
+  # Save the longitude latitude data in an object and return it. 
+  geocode_data <- data.frame(3)
+  geocode_data$location <- location
+  geocode_data$latitude <- my_content_from_json$lat
+  geocode_data$longitude <- my_content_from_json$lon
+  
+  return(geocode_data)
+  
+}
 
 
 # The following function takes the location (in string format) and your apiKey (in string format) as input.
@@ -25,10 +66,13 @@ my_api_key <- "insert your api key here"
 
 get_weather <- function(location, apiKey) {
   
-  # first, we need to use tidygeocoder::geo_osm to get the longitude and the latitude of the desired location. 
+  # first, we need to use the function geocode(location) to get the longitude and the latitude of the desired location. 
   
-  latitude <- tidygeocoder::geo_osm(location)$lat[1]
-  longitude <- tidygeocoder::geo_osm(location)$long[1]
+  latitude <- geocode(locaion)$latitude
+  longitude <- geocode(locaion)$longitude
+  
+  # latitude <- tidygeocoder::geo_osm(location)$lat[1] # old version
+  # longitude <- tidygeocoder::geo_osm(location)$long[1] # old version 
   
   if (is.na(latitude) | is.na(longitude)) {
     
@@ -101,8 +145,13 @@ get_your_forecast("hourly", "Amsterdam, Niederlande", sys.getenv(MY_API))
 
 get_map <- function(location) {
   
-  latitude <- tidygeocoder::geo_osm(location)$lat[1]
-  longitude <- tidygeocoder::geo_osm(location)$long[1]
+  # first, we need to use the function geocode(location) to get the longitude and the latitude of the desired location. 
+  
+  latitude <- geocode(locaion)$latitude
+  longitude <- geocode(locaion)$longitude
+  
+  # latitude <- tidygeocoder::geo_osm(location)$lat[1] # old version
+  # longitude <- tidygeocoder::geo_osm(location)$long[1] # old version 
   
   if (is.na(latitude) | is.na(longitude)) {
     
@@ -134,11 +183,14 @@ get_map("Amsterdam, Niederlande")
 # The map includes icons that correspond to the current weather forecast for this location. 
 
 get_icon_map <- function(location, apiKey) {
+
+  # first, we need to use the function geocode(location) to get the longitude and the latitude of the desired location. 
   
-  # first, we need to use tidygeocoder::geo_osm to get the longitude and the latitude of the desired location. 
+  latitude <- geocode(locaion)$latitude
+  longitude <- geocode(locaion)$longitude
   
-  latitude <- tidygeocoder::geo_osm(location)$lat[1]
-  longitude <- tidygeocoder::geo_osm(location)$long[1]
+  # latitude <- tidygeocoder::geo_osm(location)$lat[1] # old version
+  # longitude <- tidygeocoder::geo_osm(location)$long[1] # old version 
   
   if (is.na(latitude) | is.na(longitude)) {
     
@@ -195,8 +247,11 @@ get_weather_image <- function(location, apiKey) {
   
   # Again, we retrieve the  long/lat of the location and the current weather.
   
-  latitude <- tidygeocoder::geo_osm(location)$lat[1]
-  longitude <- tidygeocoder::geo_osm(location)$long[1]
+  latitude <- geocode(locaion)$latitude
+  longitude <- geocode(locaion)$longitude
+  
+  # latitude <- tidygeocoder::geo_osm(location)$lat[1] # old version
+  # longitude <- tidygeocoder::geo_osm(location)$long[1] # old version 
   
   if (is.na(latitude) | is.na(longitude)) {
     
